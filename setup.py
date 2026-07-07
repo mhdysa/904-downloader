c7c8dca4068e3b6dc48af4899f888a45783b89a8
mhdysa <mhdysa526@gmail.com>
2026-07-07 12:46:51 +0330

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='904-downloader',
+    version='1.0.0',
+    description='Universal Downloader for multiple platforms',
+    author='904 Developer',
+    packages=[],
+    install_requires=[
+        'kivy==2.1.0',
+        'requests==2.31.0',
+        'pytube==15.0.0'
+    ]
+)
